71a1822784283ef2debbb37cc8c185e552035f3e
Mridul-lucifer <124557704+Mridul-lucifer@users.noreply.github.com>
2026-03-18 23:40:25 +0530

    Update db.js

--- school-mgmt-backend/src/config/db.js
+++ school-mgmt-backend/src/config/db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 // Connect using the URL provided by Neon
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectModule: require('pg'),
   dialectOptions: {
     ssl: {
       require: true,
